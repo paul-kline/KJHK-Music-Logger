42ca8fd15db5c905bc3464b362efa8f89424a359
Paul Ian Kline <PaulIanKline@gmail.com>
2015-09-02 00:39:22 -0500

    successfully submits the gravity form, however, the views on the site are not updated to reflect the programatic submissions until the log music form is submitted NON-programmatically sad face.. it's always something.

--- KJHKMusicLogger.py
+++ KJHKMusicLogger.py
@@ -3,6 +3,13 @@ import sys
 sys.path += "./"
 import NowPlayingXMLParser as xmlp
 import pythonNaughtyFilter as nf
+import hmac as h1
+import hashlib as h2
+import base64 as b64
+import urllib.parse
+import urllib.request
+import calendar
+import time
 #KJHK music auto-logger
 
 
@@ -21,7 +28,20 @@ def handleDataBurst(str):
         album = nf.filterNaughty(album)
         song = nf.filterNaughty(song)
         handleLogSong(artist,album,song,isrot)
-        
+def testGET():
+    base_url = "http://kjhk.org/web/gravityformsapi/" #'http://kjhk.org/wordpress/gravityformsapi/'
+    api_key = 'REDACTED'
+    private_key = 'REDACTED'
+    method = 'GET'
+    route = 'forms/10/entries'
+    expires =  str(calendar.timegm(time.gmtime()) + 60*60)
+    string_to_sign = api_key + ":" +  method + ":" + route + ":" + expires
+    sig = sign(string_to_sign, private_key )
+
+    url = base_url + route + '?api_key=' + api_key + '&signature=' + sig + '&expires=' + expires
+    responseData = urllib.request.urlopen(url).read()
+    print(responseData)
+
 def handleLogSong(artist,album,song,isrot):
     #TODO logging to site..
     print("handleLogSong stub method.")
@@ -29,7 +49,35 @@ def handleLogSong(artist,album,song,isrot):
     print("Album: " + album)
     print("Artist: " + artist)
     print("Is Rotation: " + str(isrot))
+
+    base_url = "http://kjhk.org/web/gravityformsapi/" 
+    api_key = 'REDACTED'
+    private_key = 'REDACTED'
+    method = 'POST'
+    route = 'entries'
+    expires =  str(calendar.timegm(time.gmtime()) + 60*60)
+    string_to_sign = api_key + ":" +  method + ":" + route + ":" + expires
+    sig = sign(string_to_sign, private_key )
+
+    url = base_url + route + '?api_key=' + api_key + '&signature=' + sig + '&expires=' + expires
+    rotName = '"6"'
+    rotVal = '"Everything Other Than Rotation"'
+    if isrot:
+        rotVal = '"Rotation"'
+        
+    artistName = '"2"'
+    albumName = '"3"'
+    trackName = '"4"'
+    jstring = '[{"form_id":"10",' + rotName + ':' + rotVal + ',' + artistName + ':"' + artist + '",' + albumName+ ':"' + album + '",' + trackName + ':"' + song + '"' + '}]'
+    responseData = urllib.request.urlopen(url,bytes(jstring,'utf-8')).read()
+    print(responseData)
+    return jstring
     
+def rawurlencode(str):
+    return urllib.parse.quote(str)
+apik = 'apik'
+def sign(obj,k):   
+    return rawurlencode(b64.b64encode(h1.new(bytes(k,'UTF-8'),bytes(obj,'utf-8'), h2.sha1).digest()))
     
 if __name__ == "__main__":
 #    import sys
